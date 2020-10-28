2aa7539cf954a2088796a6cbc879c46a25dc3c66
Gabrielly <55569350+gabriellytrajano@users.noreply.github.com>
2020-10-28 03:50:35 -0300

    Create desafio.js

---
+++ desafio.js
@@ -0,0 +1,27 @@
+//Input: "fun&!! time"
+//Output: time
+
+//Input: "I love dogs"
+//Output: love
+
+frase = 'I love dogs'
+
+sempontuacao= frase.replace(/[&!]/g, '')
+
+splits = sempontuacao.split(" ")
+
+function maiorpalavra(palavras){
+    maior = palavras[0]
+    for (i=0; i< palavras.length; i++){
+        if (palavras[i].length > maior.length){
+            maior = palavras[i]
+        } else maior = maior 
+    }
+    return maior
+}
+
+//console.log(maiorpalavra(splits))
+
+//console.log(splits.reduce((a,b) => a.length > b.length ? a : b))
+
+console.log(frase.split(" ").reduce((a,b) => a.length >= b.length ? a : b))
